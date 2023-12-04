80abea6fa3389d16ac8ad2c250430fb44c3af4b1
Nikolay Savichev <n.savitchev@gaijin.ru>
2023-12-04 19:42:32 +0300

    added build script that uses cmake3 (suitable for CentOS 7)

---
+++ s2_build_linux64_cmake3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# sometimes this script should be run under devtoolset prefix (e.g. for CentOS 7):
+# scl enable devtoolset-11 llvm-toolset-7.0 './s2_build_linux64_cmake3.sh'
+
+DXC_PACKAGE="DirectXShaderCompiler"
+DXC_VERSION="1.7.2207"
+
+cd "$DXC_PACKAGE"
+
+mkdir -p "build/"
+mkdir -p "build/docs/"
+cd "build/"
+
+cmake3 ".." -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -C../cmake/caches/PredefinedParams.cmake
+cmake3 --build .
+
+cd ../..
+cp $DXC_PACKAGE/build/lib/libdxcompiler.so.3.7 DXC-$DXC_VERSION/lib/linux64/libdxcompiler.so
